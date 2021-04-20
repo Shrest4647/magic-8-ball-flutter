ed898c0b799bebda43e716bd388d81fb53f03503
Sunil Shrestha <35072109+Shrest4647@users.noreply.github.com>
2021-04-20 09:36:49 +0545

    Uptodropdown

---
+++ accounting_app/lib/BottomNavBar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class BottomNavBar extends StatefulWidget {
+  @override
+  _BottomNavBarState createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
