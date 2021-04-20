513c0048ed33dc82bb26b2b0604c21db76250083
Sunil Shrestha <35072109+Shrest4647@users.noreply.github.com>
2021-04-20 18:10:53 +0545

    Removed my accounting app

--- my_accounting_app/android/app/src/main/kotlin/com/example/my_accounting_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.my_accounting_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- my_accounting_app/lib/UnknownPage.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-class UnknownPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
-}
